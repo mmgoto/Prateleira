8054c258acbd903e3eddfa96122693a049b4a0f1
Marcelo Makoto Goto <64157859+mmgoto@users.noreply.github.com>
2021-10-23 21:55:07 -0300

    adicionando componentes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
+import "./App.css";
 
 const App = () => {
-  const message = 'Hello, World!!!!'
-  return <div className="container"></div>
+  // let message = 'Hello, Woorld!!!!'
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar Programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler Livros",
+      completed: true,
+    },
+  ]);
+
+  return (
+    <>
+      <div className="container">
+      <Tasks tasks={tasks} />
+      </div>
+    </>
+  );
 };
-export default App; 
+
+export default App;
