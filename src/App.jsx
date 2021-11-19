7ae1d6e69d30618c6b6ac31e231a7056ad543ba0
Marcelo Makoto Goto <64157859+mmgoto@users.noreply.github.com>
2021-11-18 21:05:51 -0300

    0.1.1 - concluido

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
 
   const handleTaskClick = (taskId) => {
     const newTask = tasks.map((task) => {
-      if (task.id == taskId) return { ...task, completed: !task.completed };
+      if (task.id === taskId) return { ...task, completed: !task.completed };
       return task;
     });
 
@@ -57,7 +57,7 @@ const App = () => {
   };
 
   const handleTaskDeletion = (taskId) => {
-    const newTasks = tasks.filter((task) => task.id != taskId);
+    const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
   };
 
